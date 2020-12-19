a468b1d010db52ce1beb0f46aba32e667aaf12c6
Milanaden <62472483+Milanaden@users.noreply.github.com>
2020-12-19 02:14:25 +0200

    Add files via upload

---
+++ HW 1/index.js
@@ -0,0 +1,13 @@
+// let  name = 'Генератор захисного поля '
+// let  price = '1000'
+
+
+let name = (" Генератор захисного поля" );
+var price =  ' 1000' ;
+
+let text = (" Обрано")  ;
+
+let mw=(" ціна  за  штуку") ; 
+let r = (" кредитів");
+var price =  ' 2000' ;
+console.log( text + name +  mw + price +  r )
